9836f7572ba5c150406ae1973b0abf27f2cae396
amineabdel <33087416+amineabdel@users.noreply.github.com>
2018-08-20 16:49:48 +0200

    nieuwe service erbij

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -12,7 +12,7 @@ angular.module('pokemonApp', ['ngRoute'])
 	        .otherwise({redirectTo:'/home'});
 	})
 	
-	.controller('homeCtrl', function($scope,$http,getPutSrv/*,pokSrv*/) {
+	.controller('homeCtrl', function($scope,$http,getPutSrv,pokSrv) {
 		//ik krijg errors in console omdat sommige pokemons wel al bestaan
 		// if maken zodat wanneer de pokemon al in de DB zit, niet meer in te doen
 		
@@ -59,9 +59,30 @@ angular.module('pokemonApp', ['ngRoute'])
 		
 	
    
-  /* .service('pokSrv', function($http, $q) {
-
-    })*/
+   .service('pokSrv', function($http, $q) {
+		$('#pokDateText').val('1998-01-23');
+			$('#pokDateDateText').val('2015-07-11');
+          $('#searchButton').on('click', function() {
+        	  getPutSrv.getPok().then(function(data){
+      			var dataJsonPok = data.rows["0"].doc.pokemonJson.docs;
+    			for(var i = 0; i < dataJsonPok.length; i++ ){
+    				var oudsteArr = [];
+    				var owned = dataJsonPok[i].owned;
+    				var naamPok = dataJsonPok[i].name;	
+    				var oudste=  $('#pokDateText').val();
+    				var nieuwste = $('#pokDateDateText').val();
+    				oudsteArr.push(owned);
+    				
+    				//oudsteArr.sort( function (a,b) { return a === b ? 0 : a < b ? -1: 1} );
+    				//c.log(oudsteArr);
+    				
+    				
+        			/*if(oudste == oudsteArr ){
+        				c.log(oudsteArr);
+        			}else{
+        				c.log('no');
+        			}*/
+    })
 	
 	
 	  .service('getPutSrv', function($http, $q){
